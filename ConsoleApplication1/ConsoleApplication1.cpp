9664f14d1b267696814c2bd3a8f815ebee83694b
ImAClownAndDontComplain <lukporey1317@bk.ru>
2021-11-21 21:25:30 +0500

    descendant class, checking the work of constructors and destructors
    Signed-off-by: ImAClownAndDontComplain <lukporey1317@bk.ru>

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,16 +13,16 @@ public:
 		name = "default", age = 0;
 		printf("Something()\n");
 	}
-	Something(string name, int corners) {
+	Something(string name, int age) {
 		printf("Something(string name, int age)\n");
 		this->name = name, this->age=age;
 	}
 	Something(const Something&S) {
 		name = S.name, age = S.age;
-		printf("Shape(const Shape &S)\n");
+		printf("Something(const Something &S)\n");
 	}
 	~Something() {
-		cout << "~Shape()\nit was a " << age << "-year-old " << name << endl;
+		cout << "\n~Someone()\nit was a " << age << "-year-old " << name << endl;
 	}
 	void rename(string s) {
 		name = s;
@@ -32,6 +32,34 @@ public:
 void Something::resetC() {
 	age = 0;
 }
+
+string liveliness(bool alive) {
+	if (alive == true)return "alive ";
+	else return "lifeless ";
+}
+
+class Someone : public Something {
+private: bool alive;
+public:
+	Someone() : Something() {
+		printf("Someone()\n");
+		alive = true;
+	}
+	Someone(string name, int age, bool alive) :Something(name, age) {
+		printf("Someone(string name, int age, bool alive)\n");
+		this->alive = alive;
+	}
+	Someone(const Someone& S) {
+		name = S.name, age = S.age, alive = S.alive;
+		printf("Someone(const Someone &S)\n");
+	}
+	~Someone() {
+		cout << "\n~Someone()\nit was a " << age << "-year-old and " << liveliness(alive) << name << endl;
+	}
+	void changeliveliness(bool alive) {
+		this->alive = alive;
+	}
+};
 
 class CoupleOfShapes {
 private:
@@ -43,8 +71,9 @@ public:
 
 int main()
 {
-	Something* A = new Something("a", 1);
+	Someone* A = new Someone("a", 1,true);
 	A->rename("aa");
 	A->resetC();
+	A->changeliveliness(false);
 	delete A;
 }
